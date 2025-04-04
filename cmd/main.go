0040b0a100eae744680ce935d62442c8eb747d19
Stanislav Shamilov <shamilovstas@protonmail.com>
2025-04-04 14:12:50 +0300

    adds support for motd fragments

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"log/slog"
 	"motd-joke/internal/joke"
 	"motd-joke/internal/motd"
@@ -14,17 +13,9 @@ func main() {
 	if err != nil {
 		slog.Error("cannot get top joke", slog.Any("error", err))
 	}
-
-	motdFile, err := motd.MotdFile()
-
-	if err != nil {
-		slog.Error("cannot open /etc/motd", slog.Any("error", err))
-	}
-
 	wrapped := textwrap.NewMinLines().Wrap(joke.String(), 40)
 	t := table.Draw(wrapped, table.Border{HorizontalSymbol: '-', VerticalSymbol: '|'}, 1)
-	if _, err := motdFile.WriteString(t); err != nil {
-		slog.Error("cannot write to /etc/motd", slog.Any("error", err))
+	if err := motd.CreateMotdfile(t); err != nil {
+		slog.Error("cannot write to motd file", slog.Any("error", err))
 	}
-	fmt.Println(joke.String())
 }
--- internal/motd/motd.go
+++ internal/motd/motd.go
@@ -1,8 +1,77 @@
 package motd
 
-import "os"
+import (
+	"errors"
+	"fmt"
+	"io/fs"
+	"log/slog"
+	"os"
+	"path/filepath"
+	"text/template"
+)
 
-func MotdFile() (*os.File, error) {
-	motd, err := os.Create("/etc/motd")
-	return motd, err
+const motdFragmentFilename = "99-joke-of-the-day"
+
+func CreateMotdfile(message string) error {
+	err := tryCreateGenericMotdFragment(message)
+	if errors.Is(err, fs.ErrNotExist) {
+		err = tryCreateDebianMotdFragment(message)
+
+		if errors.Is(err, fs.ErrNotExist) {
+			slog.Error("debian like fragment failed", slog.Any("error", err))
+			err = createDefaultMotd(message)
+		}
+	}
+	return err
+}
+
+func tryCreateGenericMotdFragment(message string) error {
+	dirPath := "/etc/motd.d"
+	fileInfo, err := os.Stat(dirPath)
+
+	if err == nil {
+		if !fileInfo.IsDir() {
+			return fmt.Errorf("%s not a dir: %w", dirPath, err)
+		}
+		filename := fmt.Sprintf("%s.motd", motdFragmentFilename)
+		path := filepath.Join(dirPath, filename)
+		return os.WriteFile(path, []byte(message), 0644)
+	} else {
+		slog.Error("cannot stat /etc/motd.d", slog.Any("error", err))
+		return err
+	}
+}
+
+func tryCreateDebianMotdFragment(message string) error {
+	slog.Info("creating debian-like motd fragment")
+	dirPath := "/etc/update-motd.d"
+	fileInfo, err := os.Stat(dirPath)
+
+	if err == nil {
+		if !fileInfo.IsDir() {
+			return fmt.Errorf("%s not a dir: %w", dirPath, err)
+		}
+		path := filepath.Join(dirPath, motdFragmentFilename)
+		motd, motdErr := os.Create(path)
+		if motdErr != nil {
+			return motdErr
+		}
+		motdErr = os.Chmod(path, 0755)
+		if motdErr != nil {
+			return motdErr
+		}
+		tpl, tplErr := template.ParseFiles("/usr/share/motd-joke/debian-motd-joke.tpl")
+
+		if tplErr != nil {
+			return tplErr
+		}
+		return tpl.Execute(motd, message)
+	} else {
+		return err
+	}
+}
+
+func createDefaultMotd(message string) error {
+	slog.Info("writing to /etc/motd")
+	return os.WriteFile("/etc/motd", []byte(message), 0644)
 }
